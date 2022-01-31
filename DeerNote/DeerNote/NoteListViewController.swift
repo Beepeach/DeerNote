e323f270db124ff4bc6a4174054182a087808edc
Beepeach <eh7457303j@gmail.com>
2022-01-31 16:22:00 +0900

    Refact: NoteListVC 코드 리팩토링
    코드를 보기좋게 리팩토링했습니다.

--- DeerNote/DeerNote/NoteListViewController.swift
+++ DeerNote/DeerNote/NoteListViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class NoteListViewController: UIViewController {
+    // MARK: @IBOutlet
     @IBOutlet weak var settingVCLeadingConstraint: NSLayoutConstraint!
     
+    // MARK: @IBAction
     @IBAction func tapSetting(_ sender: UIBarButtonItem) {
         UIView.animate(withDuration: 0.3) {
             self.settingVCLeadingConstraint.constant = self.settingVCLeadingConstraint.constant == 0 ? -self.view.frame.width * 0.8 : 0
             self.view.layoutIfNeeded()
         }
-        
     }
     
+    // MARK: ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setInitialLayout()
+        configureSearchBar()
+    }
+    
+    private func setInitialLayout() {
+        self.settingVCLeadingConstraint.constant = -self.view.frame.width * 0.8
+    }
+    
+    private func configureSearchBar() {
         let searchBar = UISearchController(searchResultsController: nil)
         self.navigationItem.searchController = searchBar
-        
-        self.settingVCLeadingConstraint.constant = -self.view.frame.width * 0.8
     }
 }
