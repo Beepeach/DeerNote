256037dc76a05541d6a420339edce002d59e77c7
Beepeach <eh7457303j@gmail.com>
2022-02-13 00:32:17 +0900

    Feat: TextView의 right Inset을 추가
    Scroll Indicator와 겹치지 않기하기위해서 inset을 추가시켰습니다.

--- DeerNote/DeerNote/NoteEditor/NoteEditorViewController.swift
+++ DeerNote/DeerNote/NoteEditor/NoteEditorViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class NoteEditorViewController: UIViewController {
 
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var EndEditBarButtonItem: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationController?.navigationBar.tintColor = .systemTeal
+        
+        contentTextView.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 12)
     }
 }
