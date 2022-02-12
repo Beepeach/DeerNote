11c226706abebc468296120ab66a76ea21bc887a
Beepeach <eh7457303j@gmail.com>
2022-02-12 23:45:18 +0900

    Feat: NoteEditorVC의 네비게이션바 tintcolor 설정

---
+++ DeerNote/DeerNote/NoteEditor/NoteEditorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NoteEditorViewController.swift
+//  DeerNote
+//
+//  Created by JunHeeJo on 2/12/22.
+//
+
+import UIKit
+
+class NoteEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.navigationBar.tintColor = .systemTeal
+    }
+}
