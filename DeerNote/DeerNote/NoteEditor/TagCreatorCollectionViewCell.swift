f32c64a9ce4e12b788e6874f36e61bad25928636
Beepeach <eh7457303j@gmail.com>
2022-02-14 01:19:54 +0900

    Refact: TagCreatorCollectionViewCell 코드 정리

--- DeerNote/DeerNote/NoteEditor/TagCreatorCollectionViewCell.swift
+++ DeerNote/DeerNote/NoteEditor/TagCreatorCollectionViewCell.swift
@@ -9,7 +9,4 @@ import UIKit
 
 class TagCreatorCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var TagField: UITextField!
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
 }
