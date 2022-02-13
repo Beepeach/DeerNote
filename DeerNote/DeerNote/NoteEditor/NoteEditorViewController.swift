f6e339996be4e7fd621f848906edf033fa2e8c91
Beepeach <eh7457303j@gmail.com>
2022-02-13 16:05:28 +0900

    Refact: NoteEditorVC 코드 리팩토링
    Resolves: #11

--- DeerNote/DeerNote/NoteEditor/NoteEditorViewController.swift
+++ DeerNote/DeerNote/NoteEditor/NoteEditorViewController.swift
@@ -8,17 +8,32 @@
 import UIKit
 
 class NoteEditorViewController: UIViewController {
+    // MARK: @IBOutlet
     @IBOutlet weak var contentTextView: UITextView!
     @IBOutlet weak var EndEditBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var tagViewBottomConstraint: NSLayoutConstraint!
     
+    
+    // MARK: VCLifeCycle
+    override func viewWillDisappear(_ animated: Bool) {
+        // TODO: - 나가지면 note를 저장하는 코드를 구현해야합니다.
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        navigationController?.navigationBar.tintColor = .systemTeal
+        setupDefaultApperance()
+        adjustApperanceWhenKeyboardShow()
+        resetApperanceWhenKeyboardHide()
         
+        contentTextView.becomeFirstResponder()
+    }
+    
+    private func setupDefaultApperance() {
+        navigationController?.navigationBar.tintColor = .systemTeal
         contentTextView.textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 12)
-                                    
+    }
+    
+    private func adjustApperanceWhenKeyboardShow() {
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { noti in
             guard let userInfo = noti.userInfo else {
                 return
@@ -27,6 +42,7 @@ class NoteEditorViewController: UIViewController {
             guard let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else {
                 return
             }
+            
             UIView.animate(withDuration: 0.3) { [weak self] in
                 guard let safeAreaBottomInset = self?.view.safeAreaInsets.bottom else {
                     return
@@ -35,22 +51,20 @@ class NoteEditorViewController: UIViewController {
                 self?.view.layoutIfNeeded()
             }
         }
-        
+    }
+    
+    private func resetApperanceWhenKeyboardHide() {
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
             UIView.animate(withDuration: 0.3) { [weak self] in
                 self?.tagViewBottomConstraint.constant = 0
                 self?.view.layoutIfNeeded()
             }
         }
-        
-        contentTextView.becomeFirstResponder()
     }
     
+    
+    // MARK: @IBAction
     @IBAction func tapEndEditButton(_ sender: UIBarButtonItem) {
         contentTextView.resignFirstResponder()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        print("disappear")
     }
 }
