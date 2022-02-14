0b2024ef3d597b3051f4604ab416e2bed783ee7a
Beepeach <eh7457303j@gmail.com>
2022-02-15 00:47:01 +0900

    Feat: TrashVC에 swipe 액션을 추가

--- DeerNote/DeerNote/Trash/TrashViewController.swift
+++ DeerNote/DeerNote/Trash/TrashViewController.swift
@@ -9,15 +9,16 @@ import UIKit
 
 class TrashViewController: UIViewController {
     var deletedNotes: [Note] = [
-        Note(contents: "잘있어요", tag: [], date: Date(), updatedDate: Date(), isDeleted: false),
-        Note(contents: "더미더미", tag: [], date: Date(), updatedDate: Date(), isDeleted: false),
-        Note(contents: "미더미더", tag: [], date: Date(), updatedDate: Date(), isDeleted: false),
-        Note(contents: "크하하하하", tag: [], date: Date(), updatedDate: Date(), isDeleted: false),
-        Note(contents: "안녕하세요. 반갑습니다 글자를 어디서부터 끊어야할지 잘 모르겠네요", tag: [], date: Date(), updatedDate: Date(), isDeleted: false),
+        // TODO: - CoreData에서 isDeleted가 true인 것들을 가져와야합니다.
+        Note(contents: "잘있어요", tag: [], date: Date(), updatedDate: Date(), isDeleted: true),
+        Note(contents: "더미더미", tag: [], date: Date(), updatedDate: Date(), isDeleted: true),
+        Note(contents: "미더미더", tag: [], date: Date(), updatedDate: Date(), isDeleted: true),
+        Note(contents: "크하하하하", tag: [], date: Date(), updatedDate: Date(), isDeleted: true),
+        Note(contents: "안녕하세요. 반갑습니다 글자를 어디서부터 끊어야할지 잘 모르겠네요", tag: [], date: Date(), updatedDate: Date(), isDeleted: true),
         Note(contents: """
              반가워요
              이걸 어디서 끊어야할까요
-             """, tag: [], date: Date(), updatedDate: Date(), isDeleted: false)
+             """, tag: [], date: Date(), updatedDate: Date(), isDeleted: true)
     ]
     
     @IBOutlet weak var deletedNotesTableView: UITableView!
@@ -65,5 +66,33 @@ extension TrashViewController: UITableViewDataSource {
         }
         cell.contentsLabel.text = deletedNotes[indexPath.row].contents
         return cell
+    }
+}
+
+
+extension TrashViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "삭제") { action, view, completion in
+            self.deletedNotes.remove(at: indexPath.row)
+            self.deletedNotesTableView.deleteRows(at: [indexPath], with: .automatic)
+            // TODO: - CoreData에서 삭제하는 코드가 들어가야합니다.
+            completion(true)
+        }
+        deleteAction.image = UIImage(systemName: "trash.slash")
+        
+        let restoreAction = UIContextualAction(style: .normal, title: "복구") { action, view, completion in
+            self.deletedNotes[indexPath.row].isDeleted = false
+            self.deletedNotes.remove(at: indexPath.row)
+            self.deletedNotesTableView.deleteRows(at: [indexPath], with: .automatic)
+            // TODO: - CoreData에 deleted 속성을 변경시키는 코드가 들어가야합니다.
+            completion(true)
+        }
+        restoreAction.backgroundColor = .systemGreen
+        restoreAction.image = UIImage(systemName: "arrow.clockwise")
+        
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction, restoreAction])
+        
+        return configuration
     }
 }
