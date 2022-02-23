fca4cece41344f49aa6cbe03e8de37f0bd9778d5
Beepeach <eh7457303j@gmail.com>
2022-02-23 16:16:10 +0900

    Refact: NoteInfoVC 코드 리팩토링

--- DeerNote/DeerNote/NoteInfo/NoteInfoTableViewController.swift
+++ DeerNote/DeerNote/NoteInfo/NoteInfoTableViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class NoteInfoTableViewController: UITableViewController {
+    // MARK: Properties
     var targetNote: NoteEntity?
     private var isPinned: Bool? {
         guard let targetNote = targetNote else {
@@ -15,37 +16,40 @@ class NoteInfoTableViewController: UITableViewController {
         }
         return targetNote.customSortIndex < 0 ? true : false
     }
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy. MM. dd  HH:mm:ss"
+        
+        return formatter
+    }()
 
+    // MARK: @IBOutlet
     @IBOutlet weak var createdDateLabel: UILabel!
     @IBOutlet weak var modifiedDateLabel: UILabel!
     @IBOutlet weak var pinSwitch: UISwitch!
     
+    // MARK: VCLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let dateFormatter: DateFormatter = {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy. MM. dd  hh:mm:ss"
-            
-            return formatter
-        }()
+        setupDateLables()
+        setupPinSwitch()
+    }
+    
+    private func setupDateLables() {
         createdDateLabel.text = dateFormatter.string(for: targetNote?.createdDate)
         modifiedDateLabel.text = dateFormatter.string(for: targetNote?.modifiedDate)
-        
+    }
+    
+    private func setupPinSwitch() {
         guard let isPinned = isPinned else {
             return
         }
 
         pinSwitch.isOn = isPinned
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
     }
+    
 
-    // MARK: - Table view data source
-
+    // MARK: - TableViewDataSource
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 3
     }
@@ -62,60 +66,4 @@ class NoteInfoTableViewController: UITableViewController {
             return 0
         }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
