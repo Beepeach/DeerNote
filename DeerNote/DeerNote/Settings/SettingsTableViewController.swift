e8616f3d66fd505b8136ab15bb31711748d48913
Beepeach <eh7457303j@gmail.com>
2022-02-27 23:05:37 +0900

    Fix: Setting의 글씨 크기와 색상을 통일

--- DeerNote/DeerNote/Settings/SettingsTableViewController.swift
+++ DeerNote/DeerNote/Settings/SettingsTableViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.tintColor = .systemTeal
+        
+        versionLabel.text = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
     }
 }
