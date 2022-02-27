803176738df7d91c9ccbc27ee6cfc5a449b839cd
Beepeach <eh7457303j@gmail.com>
2022-02-28 03:47:58 +0900

    Feat: Dartmode 옵션 적용

--- DeerNote/DeerNote/Predicated/SceneDelegate.swift
+++ DeerNote/DeerNote/Predicated/SceneDelegate.swift
@@ -13,6 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        setDarkMode()
+    }
+    
+    private func setDarkMode() {
+        window?.overrideUserInterfaceStyle = UserDefaults.standard.bool(forKey: "darkModeIsOn") == true ? .dark : .light
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- DeerNote/DeerNote/Settings/SettingsTableViewController.swift
+++ DeerNote/DeerNote/Settings/SettingsTableViewController.swift
@@ -8,13 +8,31 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
-    
     @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    
+    @IBAction func toggleDarkModeSwitch(_ sender: UISwitch) {
+        let userInterfaceStyle: Bool = sender.isOn
+        UserDefaults.standard.setValue(userInterfaceStyle, forKey: "darkModeIsOn")
+        self.view.window?.overrideUserInterfaceStyle = userInterfaceStyle ? .dark : .light
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+        setupVersionLabel()
+        setupDarkModeSwitch()
+    }
+    
+    private func setupNavigationBar() {
         navigationController?.navigationBar.tintColor = .systemTeal
-        
+    }
+    
+    private func setupVersionLabel() {
         versionLabel.text = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    
+    private func setupDarkModeSwitch() {
+        darkModeSwitch.isOn = UserDefaults.standard.bool(forKey: "darkModeIsOn")
     }
 }
