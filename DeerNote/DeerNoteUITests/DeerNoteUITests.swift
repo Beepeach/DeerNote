3258d203af282d878e561f4744e45f2773a665f2
Beepeach <eh7457303j@gmail.com>
2022-03-08 23:30:55 +0900

    Test: UITest Case 추가
    Textview를 인식하지 못하는 문제가 있어서 해당 문제 해결후에 다시 작성하도록 하겠습니다.
    
    Resovles: #29

--- DeerNote/DeerNoteUITests/DeerNoteUITests.swift
+++ DeerNote/DeerNoteUITests/DeerNoteUITests.swift
@@ -8,26 +8,47 @@
 import XCTest
 
 class DeerNoteUITests: XCTestCase {
+    var app: XCUIApplication!
+    var noteBeforeCount: Int!
+    var noteAfterCount: Int!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        try super.setUpWithError()
+        app = XCUIApplication()
+        app.launch()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super .tearDownWithError()
+        app = nil
+        noteBeforeCount = 0
+        noteAfterCount = 0
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_noteListViewController_whenTapNoteBarButton_shouldPresentNoteEditorViewController() {
+        givenEnterNoteEditorViewController()
+        
+        let navigationBar = app.navigationBars["New"].firstMatch
+        
+        XCTAssertTrue(navigationBar.exists)
+    }
+    
+    private func givenEnterNoteEditorViewController() {
+        app.navigationBars["All"].buttons["addNote"].tap()
+    }
+    
+    func test_noteEditorViewControoler_whenEnter_shouldKeyboardShow() {
+        givenEnterNoteEditorViewController()
+        
+        XCTAssertTrue(app.keyboards.count == 1)
+    }
+    
+    func test_noteEditorViewController_whenTapKeyboardHideButtonKeyboardShowing_shouldHideKeyboard() {
+        givenEnterNoteEditorViewController()
+        
+        app.navigationBars["New"].buttons["keyboardHide"].tap()
+        
+        XCTAssertTrue(app.keyboards.count == 0)
     }
 }
