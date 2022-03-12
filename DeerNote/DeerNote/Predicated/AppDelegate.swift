e6a57dfdb18600774e6e6ca98f4951b8b9bc7356
Beepeach <eh7457303j@gmail.com>
2022-03-12 18:45:07 +0900

    Add: Firebase realtimeDB 연결 및 의존성 추가

--- DeerNote/DeerNote/Predicated/AppDelegate.swift
+++ DeerNote/DeerNote/Predicated/AppDelegate.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         CoreDataManager.shared.setup(modelName: "DeerNote")
         return true
     }
