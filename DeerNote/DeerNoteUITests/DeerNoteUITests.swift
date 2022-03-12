874586a9ccced759f458566a0338c43f03c7f6f2
Beepeach <eh7457303j@gmail.com>
2022-03-13 03:52:36 +0900

    Test: Tag에 관한 UITest 추가

--- DeerNote/DeerNoteUITests/DeerNoteUITests.swift
+++ DeerNote/DeerNoteUITests/DeerNoteUITests.swift
@@ -9,8 +9,8 @@ import XCTest
 
 class DeerNoteUITests: XCTestCase {
     var app: XCUIApplication!
-    var noteBeforeCount: Int!
-    var noteAfterCount: Int!
+    var beforeCount: Int!
+    var afterCount: Int!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
@@ -22,8 +22,8 @@ class DeerNoteUITests: XCTestCase {
     override func tearDownWithError() throws {
         try super .tearDownWithError()
         app = nil
-        noteBeforeCount = 0
-        noteAfterCount = 0
+        beforeCount = 0
+        afterCount = 0
     }
     
     func test_noteListViewController_whenTapNoteBarButton_shouldPresentNoteEditorViewController() {
@@ -50,5 +50,71 @@ class DeerNoteUITests: XCTestCase {
         app.navigationBars["New"].buttons["keyboardHide"].tap()
         
         XCTAssertTrue(app.keyboards.count == 0)
+    }
+    
+    func test_noteEditorViewController_whenNewNote_shouldTagCellCountIsOne() {
+        givenEnterNoteEditorViewController()
+        
+        let tagTextField = app.textFields["tapTextField"]
+        let tagCollectionView = app.collectionViews["tagCollectionView"]
+        beforeCount = tagCollectionView.cells.count
+        tagTextField.tap()
+        
+        XCTAssertEqual(beforeCount, 1)
+    }
+    
+    func test_noteEditorViewController_whenAddTag_shouldTagCellCountIncrease() {
+        givenEnterNoteEditorViewController()
+        
+        let tagTextField = app.textFields["tapTextField"]
+        let tagCollectionView = app.collectionViews["tagCollectionView"]
+        beforeCount = tagCollectionView.cells.count
+        tagTextField.tap()
+     
+        tagTextField.typeText("Tag1")
+        tagTextField.typeText("\n")
+        tagTextField.typeText("Tag2")
+        tagTextField.typeText("\n")
+        
+        afterCount = tagCollectionView.cells.count
+        
+        XCTAssertEqual(beforeCount + 2, afterCount)
+    }
+    
+    func test_noteEditorViewController_whenAddExistedTag_shouldTagCellCountIncrease() {
+        givenEnterNoteEditorViewController()
+        
+        let tagTextField = app.textFields["tapTextField"]
+        let tagCollectionView = app.collectionViews["tagCollectionView"]
+        beforeCount = tagCollectionView.cells.count
+        tagTextField.tap()
+     
+        tagTextField.typeText("Tag1")
+        tagTextField.typeText("\n")
+        tagTextField.typeText("Tag1")
+        tagTextField.typeText("\n")
+        
+        afterCount = tagCollectionView.cells.count
+        
+        XCTAssertEqual(beforeCount + 1, afterCount)
+    }
+    
+    func test_noteEditorViewController_whenRemoveTag_shouldTagCellCountDecrease() {
+        givenEnterNoteEditorViewController()
+        
+        let tagTextField = app.textFields["tapTextField"]
+        let tagCollectionView = app.collectionViews["tagCollectionView"]
+        let removeButton = app.buttons["tagDeleteButton"]
+        beforeCount = tagCollectionView.cells.count
+        tagTextField.tap()
+     
+        tagTextField.typeText("Tag1")
+        tagTextField.typeText("\n")
+        removeButton.tap()
+        
+        
+        afterCount = tagCollectionView.cells.count
+        
+        XCTAssertEqual(beforeCount, afterCount)
     }
 }
